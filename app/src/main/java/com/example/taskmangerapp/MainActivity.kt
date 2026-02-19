f86a377741af6f557fbf015e24f35fad4c900e3a
Allen <Allenpmraz@gmail.com>
2026-02-19 17:27:10 -0500

    Added a check if input was blanck to stop it from creating blanck tasks

--- app/src/main/java/com/example/taskmangerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmangerapp/MainActivity.kt
@@ -5,25 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.taskmangerapp.Task
 import com.example.taskmangerapp.ui.theme.TaskMangerAppTheme
 
 
@@ -36,9 +22,9 @@ class MainActivity : ComponentActivity() {
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
                 ){
-                    TaskManagerApp()
+                    TaskManagerApp() // calls task manager app
                 }
             }
         }
--- app/src/main/java/com/example/taskmangerapp/TaskManagerApp.kt
+++ app/src/main/java/com/example/taskmangerapp/TaskManagerApp.kt
@@ -1,7 +1,6 @@
 package com.example.taskmangerapp
 
-import android.R
-import android.graphics.Color
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -17,6 +15,7 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -32,63 +31,80 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 
+// the function that calls the other functions so it can build the design
 @Composable
 fun TaskManagerApp(modifier: Modifier = Modifier,
                    taskViewModel: TaskViewModel = viewModel()
 ){
-    Column(modifier = modifier){
-        AddTask(
-            list = taskViewModel.tasks,
-            onAddTask = {task -> taskViewModel.add(task)}
-        )
+    Scaffold {
+        Column(
+            modifier = modifier.padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(modifier = modifier.padding(16.dp).fillMaxWidth(), // the top of the app
+                ){
+                Text(
+                    text = "Task Manager",
+                    style = MaterialTheme.typography.headlineLarge
 
-        TaskList(
-            list = taskViewModel.tasks,
-            onCheckedTask = { task, checked ->
-               taskViewModel.changeTaskChecked(task, checked)
-            },
-            onCloseTask = { task ->
-                taskViewModel.remove(task)
+                )
             }
-        )
+            AddTask( //calls AddTask
+                list = taskViewModel.tasks,
+                onAddTask = { task -> taskViewModel.add(task) }
+            )
+
+            TaskList( // calls TaskList
+                list = taskViewModel.tasks,
+                onCheckedTask = { task, checked ->
+                    taskViewModel.changeTaskChecked(task, checked)
+                },
+                onCloseTask = { task ->
+                    taskViewModel.remove(task)
+                }
+            )
+        }
     }
 
 }
 
+
+
+// Function that lets the user enter a task name and add it to the list
 @Composable
 fun AddTask (list: List<Task>, onAddTask: (Task) -> Unit, modifier: Modifier = Modifier){
-    var input by remember {   mutableStateOf<String>("") }
-    var id  by remember { mutableIntStateOf(list.size) }
+    var input by remember {   mutableStateOf("") } // value that will be converted into a task
+    var id  by remember { mutableIntStateOf(list.size) } // value that stores the key id
     Row(modifier = modifier.padding(16.dp)) {
         TextField(
             value = input,
             onValueChange = { input = it },
             modifier = modifier
         )
-        Button(onClick = { onAddTask( Task(id,input))
+        Button(onClick = {  if(input != ""){
+                            onAddTask( Task(id,input))
                             input = ""
-                            id = list.size + 1},
+                            id = list.size + 1}
+                            }, // stores the input and id into a task in the list and checks to see if input is black or not
                 modifier = modifier) {
             Text("Add Task")
         }
     }
 }
 
-
-
+// Function that is used to design the individual elements in the list
 @Composable
 fun TaskManagerCard(taskName: String,
                     checked: Boolean,
                     onCheckedChange: (Boolean) -> Unit,
                     onClose: () -> Unit,
                     modifier: Modifier = Modifier){
-    var color = MaterialTheme.colorScheme.onBackground
+    var color = MaterialTheme.colorScheme.onBackground // stores the color to be used by the text
 
-    if(checked){
+    if(checked){ // checks to see if element has been checked and will change the color of the text
         color = MaterialTheme.colorScheme.error
     }
 
-    Card(modifier = modifier,) {
+    Card(modifier = modifier.padding(8.dp)) {
         Row(
             modifier = modifier.fillMaxWidth()
                 .clip(MaterialTheme.shapes.small)
@@ -118,14 +134,15 @@ fun TaskManagerCard(taskName: String,
 
 }
 
+// Function that creates a LazyColumn and add scrollobility
 @Composable
 fun TaskList(list: List<Task>,
              onCheckedTask: (Task, Boolean) -> Unit,
              onCloseTask: (Task) -> Unit,
              modifier: Modifier = Modifier){
-    LazyColumn(modifier = modifier) {
+    LazyColumn(modifier = modifier.padding(16.dp)) {
         items(list, key = { task -> task.id}){ task ->
-            TaskManagerCard(
+            TaskManagerCard( // calls TaskManagerCard to stylize the individual elements
                 taskName = task.label,
                 checked = task.checked,
                 onCheckedChange = { checked -> onCheckedTask(task, checked)},
